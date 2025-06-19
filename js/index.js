f4a196ae03625ec9340366b881ef06c584ba73ed
@Gen639 <evgeny.goncharov.pro@gmail.com>
2025-06-19 22:31:39 +0200

    Minor changes

--- js/index.js
+++ js/index.js
@@ -1,38 +1,40 @@
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("navLinks");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("navLinks");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-});
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
 
-let currentSlide = 0;
-const track = document.querySelector(".carousel-track");
-const slides = document.querySelectorAll(".carousel-item");
+  let currentSlide = 0;
+  const track = document.querySelector(".carousel-track");
+  const slides = document.querySelectorAll(".carousel-item");
 
-function moveToSlide(index) {
-  currentSlide = index % slides.length;
-  track.style.transform = `translateX(-${currentSlide * 100}%)`;
-}
-
-setInterval(() => {
-  moveToSlide(currentSlide + 1);
-}, 10000);
-
-track.addEventListener("click", () => {
-  moveToSlide(currentSlide + 1);
-});
-
-function randomlyHighlightUseCases() {
-  const boxes = document.querySelectorAll(".usecase-box");
-  let previous;
+  function moveToSlide(index) {
+    currentSlide = index % slides.length;
+    track.style.transform = `translateX(-${currentSlide * 100}%)`;
+  }
 
   setInterval(() => {
-    if (previous) previous.classList.remove("active");
+    moveToSlide(currentSlide + 1);
+  }, 10000);
 
-    const randomIndex = Math.floor(Math.random() * boxes.length);
-    boxes[randomIndex].classList.add("active");
-    previous = boxes[randomIndex];
-  }, 1000);
-}
+  track.addEventListener("click", () => {
+    moveToSlide(currentSlide + 1);
+  });
 
-randomlyHighlightUseCases();
+  function randomlyHighlightUseCases() {
+    const boxes = document.querySelectorAll(".usecase-box");
+    let previous;
+
+    setInterval(() => {
+      if (previous) previous.classList.remove("active");
+
+      const randomIndex = Math.floor(Math.random() * boxes.length);
+      boxes[randomIndex].classList.add("active");
+      previous = boxes[randomIndex];
+    }, 1000);
+  }
+
+  randomlyHighlightUseCases();
+});
